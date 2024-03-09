33d79f7174d03108e95b9b467e5f466b93fda0b8
BaigaUsu <baigazy7777@gmail.com>
2024-03-10 03:51:14 +0600

    infinte scroll

--- js/app.js
+++ js/app.js
@@ -55,3 +55,36 @@ form.addEventListener("submit", (e) => {
         search.value = "";
     }
 });
+
+const slider = document.querySelector('.video-list');
+const rightScroll = document.getElementById('rightScroll');
+const leftScroll = document.getElementById('leftScroll');
+const videoElements = document.querySelectorAll('.video-list video');
+let flag = true
+
+const videoArray = Array.from(videoElements);
+console.log(videoArray)
+
+let activeVideo = 0
+
+rightScroll.addEventListener('click', () => {
+    activeVideo++;
+    if (activeVideo >= videoArray.length) activeVideo = 0;
+
+    const removedVideo = document.querySelector('.video-list video');
+    removedVideo.remove();
+
+    const newVideo = videoArray[activeVideo].cloneNode(true);
+    slider.append(newVideo);
+});
+
+leftScroll.addEventListener('click', () => {
+    activeVideo--;
+    if (activeVideo < 0) activeVideo = videoArray.length - 1;
+
+    const removedVideo = document.querySelector('.video-list video');
+    removedVideo.remove();
+
+    const newVideo = videoArray[activeVideo].cloneNode(true);
+    slider.appendChild(newVideo);
+});
