2ba85d4694b51811bef2df16a3583b849d16a6ec
BaigaUsu <baigazy7777@gmail.com>
2024-03-10 04:21:24 +0600

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -60,7 +60,7 @@ const slider = document.querySelector('.video-list');
 const rightScroll = document.getElementById('rightScroll');
 const leftScroll = document.getElementById('leftScroll');
 const videoElements = document.querySelectorAll('.video-list video');
-let flag = true 
+let flag = true
 
 const videoArray = Array.from(videoElements);
 console.log(videoArray)
@@ -68,23 +68,19 @@ console.log(videoArray)
 let activeVideo = 0
 
 rightScroll.addEventListener('click', () => {
-    activeVideo++;
-    if (activeVideo >= videoArray.length) activeVideo = 0;
-
-    const removedVideo = document.querySelector('.video-list video');
-    removedVideo.remove();
-
-    const newVideo = videoArray[activeVideo].cloneNode(true);
-    slider.append(newVideo);
-});
+    slider.style.scrollBehavior = 'smooth'
+    slider.scrollLeft += 840;
+})
 
 leftScroll.addEventListener('click', () => {
-    activeVideo--;
-    if (activeVideo < 0) activeVideo = videoArray.length - 1;
+    slider.style.scrollBehavior = 'smooth'
+    slider.scrollLeft -= 840;
+})
 
-    const removedVideo = document.querySelector('.video-list video');
-    removedVideo.remove();
+const sliderWidth = slider.scrollWidth - slider.clientWidth;
 
-    const newVideo = videoArray[activeVideo].cloneNode(true);
-    slider.appendChild(newVideo);
+slider.addEventListener('scroll', () => {
+    if (slider.scrollLeft === sliderWidth) {
+        slider.scrollLeft = 0;
+    }
 });
